81dec400dd90d0866899e145509677f5af8dadcb
Vitaly Gumenyak <vetalgr@gmail.com>
2020-09-08 00:57:19 +0300

    import forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContextMenuComponent } from './context-menu/context-menu.component';
@@ -16,7 +17,8 @@ import { RateDirectionComponent } from './rate-direction/rate-direction.componen
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
